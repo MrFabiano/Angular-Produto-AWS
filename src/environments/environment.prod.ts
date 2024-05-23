282abe3c31eb3fcde957641fea759826ed2c0d96
fabiano <fabiannosanttos@gmail.com>
2024-05-23 15:34:47 -0300

    update environment prod aws or local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    url: 'http://test-apiproduto-env.eba-ugbfb8fr.us-east-2.elasticbeanstalk.com/api'
+    url: 'http://test-apiproduto-env.eba-ugbfb8fr.us-east-2.elasticbeanstalk.com/api/'
 };
