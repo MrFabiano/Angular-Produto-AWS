171eb7ea27f53246b2f99d7252b0994987bc3662
fabiano <fabiannosanttos@gmail.com>
2024-05-24 15:18:18 -0300

    configured application or yml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    url: 'http://test-apiproduto-env.eba-ugbfb8fr.us-east-2.elasticbeanstalk.com/api/'
+    url: 'http://apiproduto-spring-env.eba-enn3rsz2.us-east-2.elasticbeanstalk.com/api/'
 };
